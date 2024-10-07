c7c1c5f07c074a0c4a7e8acf8e30a332d4ed12bc
Carrick <remillardc15@gmail.com>
2024-10-06 20:43:59 -0400

    fix: update bat and exe

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,7 +3,7 @@ import app.v0.Game;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // System.out.println("hello world");
         new Game();
     }
 }
