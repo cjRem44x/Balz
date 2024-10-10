31ca7c05f01221f7c813f2189a4079ba2924e25e
Carrick <remillardc15@gmail.com>
2024-10-09 21:48:05 -0400

    feat: balls and colors

--- src/java/app/v0/Game.java
+++ src/java/app/v0/Game.java
@@ -1,12 +1,16 @@
 package app.v0;
 
 import java.awt.event.*;
+import java.util.List;
+import java.util.ArrayList;
 import javax.swing.Timer;
 import app.v0.*;
 
 public class Game {
     private Bar bar;
     private Window win;
+    private List<Ball> balls = new ArrayList<>();
+    private int num_balls = 10, ball_size = 50, ball_speed = 5;
 
     public static final int WIDTH = 800, HEIGHT = 600,
                         BAR_WIDTH = 100, BAR_HEIGHT = 20, BAR_SPEED = 90,
@@ -17,6 +21,7 @@ public class Game {
     }
 
     public void launch() {
+        createBalls();
         win = new Window();
         bar = new Bar(
             (int)((WIDTH/2.00)-(BAR_WIDTH/2.00)),
@@ -24,8 +29,7 @@ public class Game {
             BAR_WIDTH, BAR_HEIGHT
         );
         bar.vel(BAR_SPEED);
-        win.bar(bar);
-        win.barfg(255,0,0);
+        win.comp(this.bar, this.balls);
         win.winbg(0,0,0);
         win.size(WIDTH, HEIGHT);
         win.keyboard( new Keys(bar) );
@@ -43,8 +47,62 @@ public class Game {
     }
 
     public void update() {
+        barCollision();
+        ballCollision();
+        moveBalls();
+        win.repaint();
+    }
+
+    private void barCollision() {
         if (bar.x >= (WIDTH - BAR_WIDTH)) bar.x = (WIDTH - BAR_WIDTH);
         if (bar.x <= 0) bar.x = 0;
-        win.repaint();
+    }
+
+    private void ballCollision() {
+        for (int i=0; i<this.num_balls; i++) {
+            Ball b = this.balls.get(i);
+            b.borderCollision(WIDTH, HEIGHT);
+        }
+    }
+
+    private void moveBalls() {
+        for (int i=0; i<this.num_balls; i++) {
+            Ball b = this.balls.get(i);
+            b.x += b.vx;
+            b.y += b.vy;
+            b.gravitas();
+        }
+    }
+
+
+    private void inter() {
+        Corner tlbar = new Corner(this.bar.x, this.bar.y),
+            trbar = new Corner(this.bar.x+this.bar.width, this.bar.y),
+            blbar = new Corner(this.bar.x, this.bar.y+this.bar.height),
+            brbar = new Corner(this.bar.x+this.bar.width, this.bar.y+this.bar.height);
+
+        for (int i=0; i<this.ball_size; i++) {
+            Ball b = this.balls.get(i);
+            Corner tlb = new Corner(b.x, b.y),
+                trb = new Corner(b.x+b.width, b.y),
+                blb = new Corner(b.x, b.y+b.height),
+                brb = new Corner(b.x+b.width, b.y+b.height);
+        }
+
+    }
+
+    private record Corner(int x, int y) {}
+
+    private void createBalls() {
+        for (int i=0; i<this.num_balls; i++) {
+            Ball b = new Ball(this.ball_size, this.ball_size, this.ball_size, this.ball_size);
+            b.vel(this.ball_speed*rand(-1, 1), this.ball_speed*rand(-1, 1));
+            this.balls.add(b);
+        }
+    }
+
+    private int rand(int min, int max) {
+        final var RANDOM = new java.util.Random();
+        return RANDOM.nextInt((max - min) + 1) + min;
     }
 }
--- src/java/app/v0/Window.java
+++ src/java/app/v0/Window.java
@@ -2,20 +2,43 @@ package app.v0;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Window 
 extends JPanel {
     private JFrame f = new JFrame();
     private Bar bar = null;
-    private Color bar_clr;
+    private List<Ball> balls = null;
+
+    private int rmin = 25, rmax = 254, 
+                gmin = 25, gmax = 254, 
+                bmin = 25, bmax = 254;
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        if (this.balls != null) {
+            for (int i=0; i<this.balls.size(); i++) {
+                Ball b = this.balls.get(i);
+                g.setColor( new Color(
+                    rand(this.rmin, this.rmax), 
+                    rand(this.gmin, this.gmax), 
+                    rand(this.bmin, this.bmax)
+                ) );
+                g.fillOval(b.x, b.y, b.width, b.height);
+            }
+        }
         if (this.bar != null) {
-            g.setColor(bar_clr);
+            g.setColor( 
+                new Color(0, rand(50, 255), 0) 
+            );
+            g.fillRect(bar.x-3, bar.y-3, bar.width+3, bar.height+3);
+            g.setColor( 
+                new Color( rand(50, 255), 0, rand(50, 155) ) 
+            );
             g.fillRect(bar.x, bar.y, bar.width, bar.height);
         }
     }
@@ -31,12 +54,9 @@ extends JPanel {
         );
     }
 
-    public void bar(Bar b) {
-        this.bar = b;
-    }
-
-    public void barfg(int r, int b, int g) {
-        bar_clr = new Color(r, g, b);
+    public void comp(Bar bar, List<Ball> balls) {
+        this.bar = bar;
+        this.balls = balls;
     }
 
     public void winbg(int r, int g, int b) {
@@ -59,5 +79,10 @@ extends JPanel {
         f.setResizable(false);
         f.setVisible(true);
         f.setLocationRelativeTo(null);
+    }
+
+    private int rand(int min, int max) {
+        final var RANDOM = new java.util.Random();
+        return RANDOM.nextInt((max - min) + 1) + min;
     }
 }
