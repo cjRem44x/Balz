fdde9f5f360e413cb61aed11dea7b26e28005709
cjRem44x <remillardc15@gmail.com>
2025-10-09 11:22:09 -0400

    feat: add game dirver and engine

---
+++ src/main/java/game/GameDriver.java
@@ -0,0 +1,9 @@
+/************************************************************
+ * File:       GameDriver.java
+ * Author(s):  CJ Remillard (cjRem44x on Github)
+ ************************************************************/
+package main.java.game;
+
+public class GameDriver
+{
+}
---
+++ src/main/java/game/engine/GameEngine.java
@@ -0,0 +1,8 @@
+/************************************************************
+ * File:       GameEngine.java
+ * Author(s):  CJ Remillard (cjRem44x on Github)
+ ************************************************************/
+package main.java.game.engine;
+
+public class GameEngine
+{}
